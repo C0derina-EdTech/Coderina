9583c61d6c26a015e397ae13b0935baef086a4b4
VictoryK-tech <kemelevictory3802@gmail.com>
2025-11-12 00:13:32 +0100

    Remove client folder

--- client/app/lib/sanityClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const sanityClient = createClient({
-  projectId: "5tjiz4zw",
-  dataset: "production", // Or your custom dataset
-  apiVersion: "2023-01-01", // Use today's date or matching your project
-  useCdn: false, // `true` for public content, `false` if you're fetching drafts/auth content
-  token: process.env.SANITY_API_TOKEN,
-});
